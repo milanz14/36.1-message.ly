c34927a8eff8cfee158e50625f949446b7a7f34a
milanzagorac <zagoracm@gmail.com>
2021-07-12 07:41:19 -0400

    add init files

---
+++ routes/auth.js
@@ -0,0 +1,12 @@
+/** POST /login - login: {username, password} => {token}
+ *
+ * Make sure to update their last-login!
+ *
+ **/
+
+/** POST /register - register user: registers, logs in, and returns token.
+ *
+ * {username, password, first_name, last_name, phone} => {token}.
+ *
+ *  Make sure to update their last-login!
+ */
