c2d3e9676306caa8772cc2e2987dd9b038885bfd
milanzagorac <zagoracm@gmail.com>
2021-07-14 17:10:28 -0400

    WIP

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,39 @@
+const express = require("express");
+const router = express.Router();
+const ExpressError = require("../ExpressError");
+const jwt = require("jsonwebtoken");
+const { SECRET_KEY } = require("../config");
+const User = require("../models/user");
+
 /** GET / - get list of users.
  *
  * => {users: [{username, first_name, last_name, phone}, ...]}
  *
  **/
 
+router.get("/", async (req, res, next) => {
+    try {
+        let users = await User.all();
+        return res.json({ users: users });
+    } catch (e) {
+        return next(e);
+    }
+});
+
 /** GET /:username - get detail of users.
  *
  * => {user: {username, first_name, last_name, phone, join_at, last_login_at}}
  *
  **/
+
+router.get("/:username", async (req, res, next) => {
+    try {
+        let user = await User.get(username);
+        return res.json({ user: user });
+    } catch (e) {
+        return next(e);
+    }
+});
 
 /** GET /:username/to - get messages to user
  *
